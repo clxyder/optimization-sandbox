2ac0182b64d4f2fbb309598d62c90d6f6d57998d
Carlos A. Wong <carloswong54@gmail.com>
2022-05-19 21:42:55 -0400

    Cleaned up maps construct and added map and threadmap

--- maps.py
+++ maps.py
@@ -5,14 +5,12 @@ n = 2
 m = 4
 N = 6
 
-x = MX.sym('y', 2, 1) # represents the [2,1] vector in the for-loop
-X = MX.sym("X", 2, N) # the entire X matrix
+x = MX.sym('y', n, 1) # represents the [2,1] vector in the for-loop
+X = MX.sym("X", n, N) # the entire X matrix
 
 m = np.vstack((np.eye(2), np.ones([1,2]))) # the transformation matrix
-print(m)
 
 f = Function('f', [x], [mtimes(m,x)])
-
 print(f)
 
 ys = []
@@ -23,7 +21,16 @@ Y = hcat(ys)
 F = Function('F',[X],[Y])
 print(F)
 
-X1 = np.vstack(([2,3,4,5,6,7], [3,4,5,6,7,8]))
+X1 = np.vstack(([2,3,4,5,6,7],
+                [3,4,5,6,7,8]))
 print(X1)
 
 print(F(X1))
+
+# map construct
+F = f.map(N)
+print(F)
+
+# threadmap construct
+F = f.map(N,"thread",2)
+print(F)
