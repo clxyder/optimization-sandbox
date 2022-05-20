a513287a86f8d0a17cb40b7faae0921b4e0d04dc
Carlos A. Wong <carloswong54@gmail.com>
2022-05-20 11:14:17 -0400

    Added DaeBuilder system simulation

--- dae-builder.py
+++ dae-builder.py
@@ -1,8 +1,12 @@
 from casadi import (
     DaeBuilder,
-    integrator
+    integrator,
+    hcat,
+    vcat,
 )
 
+# Constants
+N = 21
 g = 9.81 # earth's gravity m/s2
 
 dae = DaeBuilder()
@@ -35,3 +39,39 @@ dae.set_unit('m','kg')
 
 # Display DAE
 dae.disp(True)
+
+### Create ODE solver function
+ode_f = dae.create('f', ['x','u','p'], ['ode'])
+print("ode_f: ", ode_f)
+
+x0 = [2,3,1]
+u0 = 2
+p0 = [2,3,u0]
+
+# Solve ODE
+r = ode_f(x0, u0, p0)
+print("x_dot: ", r)
+
+### Simulate system
+# Create integrator
+daeSetup = {
+    'x': vcat(dae.x),
+    'p': vcat([*dae.p, *dae.u]),
+    'ode': vcat(dae.ode),
+}
+
+I = integrator('F', 'idas', daeSetup)
+print(I)
+
+# Create vectors
+X = [2,3,1]
+u0 = 2
+P = [2,3,u0]
+J = 0
+
+for k in range(N):
+    P[2] = P[2] * (1/(k+1))
+    Ik = I(x0=X, p=P)
+    X = Ik['xf']
+    J += Ik['qf']
+    print(f'X: {X}\nP: {P}\nJ: {J}')
