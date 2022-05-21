005cd48753d35bd24ed9e50cae42a660de869a3b
Carlos A. Wong <carloswong54@gmail.com>
2022-05-20 20:08:29 -0400

    Extracted states & control variables from optimization variables

--- cart_pole_collocation.py
+++ cart_pole_collocation.py
@@ -23,89 +23,100 @@ dmax = 2*d
 umax = 20
 M = 5
 
-# q1 = ca.SX.sym('q1')
-# q2 = ca.SX.sym('q2')
-# q1dot = ca.SX.sym('q1dot')
-# q2dot = ca.SX.sym('q2dot')
-# u = ca.SX.sym('u')
-# x = ca.vertcat(q1,q2,q1dot,q2dot)
-# q1ddot = ( l * m2 * ca.sin(q2) * ca.power(q2dot,2) + u + m2 * g * ca.cos(q2) * ca.sin(q2) ) / ( m1 + m2 * (1 - ca.power(ca.cos(q2),2)) );
-# q2ddot = - ( l * m2 * ca.cos(q2) * ca.sin(q2) * ca.power(q2dot,2) + u * ca.cos(q2) + (m1 + m2) * g * ca.sin(q2) ) / ( l * m1 + l * m2 * (1 - ca.power(ca.cos(q2),2)) );
-# f = ca.Function('f',[x,u],[ca.vertcat(q1dot,q2dot,q1ddot,q2ddot)])
-
 def f(x,u):
     q1 = x[0,:]
     q2 = x[1,:]
     q1dot = x[2,:]
     q2dot = x[3,:]
-    q1ddot = ( l * m2 * ca.sin(q2) * ca.power(q2dot,2) + u + m2 * g * ca.cos(q2) * ca.sin(q2) ) / ( m1 + m2 * (1 - ca.power(ca.cos(q2),2)) );
-    q2ddot = - ( l * m2 * ca.cos(q2) * ca.sin(q2) * ca.power(q2dot,2) + u * ca.cos(q2) + (m1 + m2) * g * ca.sin(q2) ) / ( l * m1 + l * m2 * (1 - ca.power(ca.cos(q2),2)) );
-    return ca.vertcat(q1dot,q2dot,q1ddot,q2ddot)
+    q1ddot = (
+        l * m2 * ca.sin(q2) * ca.power(q2dot,2) + u + m2 * g * ca.cos(q2) * ca.sin(q2)
+    ) / (
+        m1 + m2 * (1 - ca.power(ca.cos(q2),2))
+    )
+    q2ddot = - (
+        l * m2 * ca.cos(q2) * ca.sin(q2) * ca.power(q2dot,2) + u * ca.cos(q2) + (m1 + m2) * g * ca.sin(q2)
+    ) / (
+        l * m1 + l * m2 * (1 - ca.power(ca.cos(q2),2))
+    )
+    return ca.vertcat(q1dot, q2dot, q1ddot, q2ddot)
 
 
-# Set up the optimisation problem
+# Set up the optimization problem
 opti = ca.Opti()
 
 p = opti.variable(M,N)
 
+# Extract variables
+x = p[:4,:]
+u = p[4,:]
+
+q1 = x[0,:]
+q2 = x[1,:]
+q1dot = x[2,:]
+q2dot = x[3,:]
+
 # Objective function
-#J = h_k/2*ca.sum2(ca.power(p[4,:-1],2) + ca.power(p[4,1:],2))
-J = h_k/2*ca.sum2(ca.power(p[4,:-1],2) + ca.power(p[4,1:],2))
+J = h_k/2*ca.sum2(ca.power(u[:-1],2) + ca.power(u[1:],2))
 opti.minimize(J)
 
 # Interpolation constraint symbolic expressions
-xk = p[:4,:-1]
-xk1 = p[:4,1:]
-uk = p[4,:-1]
-uk1 = p[4,1:]
+xk = x[:,:-1]
+xk1 = x[:,1:]
+uk = u[:-1]
+uk1 = u[1:]
+
 fk = f(xk,uk)
 fk1 = f(xk1,uk1)
 uc = (uk + uk1)/2
 xc = 1/2 * (xk + xk1) + h_k/8 * (fk - fk1)
 fc = f(xc,uc)
+
 G = xk-xk1 + h_k/6*(fk + 4*fc + fk1)
 opti.subject_to(G==0) # Add interpolation constraint
 
 
 # Add path constraints
-opti.subject_to(p[0,:]<dmax)
-opti.subject_to(-dmax<p[0,:])
-opti.subject_to(p[4,:]<umax)
-opti.subject_to(-umax<p[4,:])
+opti.subject_to(q1 < dmax)
+opti.subject_to(-dmax < q1)
+opti.subject_to(u < umax)
+opti.subject_to(-umax < u)
 
 # Add boundary constraints
-opti.subject_to(p[:4,0]==[0,0,0,0])
-opti.subject_to(p[:4,-1]==[d,ca.pi,0,0])
+opti.subject_to(x[:,0]==[0,0,0,0])
+opti.subject_to(x[:,-1]==[d,ca.pi,0,0])
 
-# Intialise
-opti.set_initial(p[0,:],ca.linspace(0,d,N))
-opti.set_initial(p[1,:],ca.linspace(0,ca.pi,N))
-opti.set_initial(p[2,:],ca.linspace(0,0,N))
-opti.set_initial(p[3,:],ca.linspace(0,0,N))
-opti.set_initial(p[4,:],ca.linspace(0,0,N))
+# Intialize
+opti.set_initial(q1[:],ca.linspace(0,d,N))
+opti.set_initial(q2[:],ca.linspace(0,ca.pi,N))
+opti.set_initial(q1dot[:],ca.linspace(0,0,N))
+opti.set_initial(q2dot[:],ca.linspace(0,0,N))
+opti.set_initial(u[:],ca.linspace(0,0,N))
 
 opti.solver('ipopt')
 sol = opti.solve()
 
-# Animate the stuff
-q1 = sol.value(p[0,:])
-q2 = sol.value(p[1,:])
-u_opt = sol.value(p[4,:])
+# Plot the solution
+q1_opt = sol.value(q1)
+q2_opt = sol.value(q2)
+u_opt = sol.value(u)
 
 t = np.linspace(0,T,N)
 
 plt.figure(1)
 
+# Plot cart position
 plt.subplot(311)
-plt.plot(t,q1)
+plt.plot(t,q1_opt)
 plt.legend(['q1'])
 plt.grid()
 
+# Plot pole angle
 plt.subplot(312)
-plt.plot(t,q2)
+plt.plot(t,q2_opt)
 plt.legend(['q2'])
 plt.grid()
 
+# Plot control law
 plt.subplot(313)
 plt.plot(t,u_opt)
 plt.legend(['u'])
